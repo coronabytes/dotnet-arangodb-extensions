e9576fb5770edf3a2ca5fb2d24734a2af76ac75b
jung <jung@corona-bytes.net>
2021-03-12 15:56:46 +0100

    Closes #4

--- Core.Arango.DevExtreme/DxLoadResult.cs
+++ Core.Arango.DevExtreme/DxLoadResult.cs
@@ -6,20 +6,27 @@ namespace Core.Arango.DevExtreme
 {
     public class DxLoadResult
     {
+        [JsonProperty("data")] 
+        [JsonPropertyName("data")]
         public object Data { get; set; }
 
+
         [DefaultValue(-1)]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        [JsonProperty("totalCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
         [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("totalCount")]
         public long TotalCount { get; set; } = -1;
 
         [DefaultValue(-1)]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        [JsonProperty("groupCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
         [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("groupCount")]
         public long GroupCount { get; set; } = -1;
 
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        
+        [JsonProperty("summary", DefaultValueHandling = DefaultValueHandling.Ignore)]
         [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("summary")]
         public decimal?[] Summary { get; set; }
     }
 }
