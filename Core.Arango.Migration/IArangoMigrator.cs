1d5841d050c1fb2e0c214718e707c1d907d85392
jung <jung@corona-bytes.net>
2024-11-30 10:52:37 +0100

    some valuetasks

--- Core.Arango.Migration/IArangoMigrator.cs
+++ Core.Arango.Migration/IArangoMigrator.cs
@@ -28,13 +28,13 @@ namespace Core.Arango.Migration
         /// <summary>
         ///  Get structure from database
         /// </summary>
-        Task<ArangoStructure> GetStructureAsync(ArangoHandle db,
+        ValueTask<ArangoStructure> GetStructureAsync(ArangoHandle db,
             CancellationToken cancellationToken = default);
 
         /// <summary>
         ///  Apply structure to database
         /// </summary>
-        Task ApplyStructureAsync(ArangoHandle db, ArangoStructure update,
+        ValueTask ApplyStructureAsync(ArangoHandle db, ArangoStructure update,
             ArangoMigrationOptions options = null);
 
         /// <summary>
@@ -50,16 +50,16 @@ namespace Core.Arango.Migration
         /// <summary>
         ///     Apply missing migrations up to latest
         /// </summary>
-        Task UpgradeAsync(ArangoHandle db);
+        ValueTask UpgradeAsync(ArangoHandle db);
 
         /// <summary>
         ///     Export database to zip archive stream
         /// </summary>
-        Task ExportAsync(ArangoHandle db, Stream output, ArangoMigrationScope scope);
+        ValueTask ExportAsync(ArangoHandle db, Stream output, ArangoMigrationScope scope);
 
         /// <summary>
         ///     Import database from zip archive stream and replace existing data
         /// </summary>
-        Task ImportAsync(ArangoHandle db, Stream input, ArangoMigrationScope scope);
+        ValueTask ImportAsync(ArangoHandle db, Stream input, ArangoMigrationScope scope);
     }
 }
