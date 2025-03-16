dc17b18e52ade9a6798ab2a1b13ecc35bfc5a846
Harry Cordewener <admin@twilightdays.org>
2025-03-16 17:36:24 -0500

    Add testing.

---
+++ Core.Arango.Serilog.Tests/SerilogUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using Serilog;
+using Serilog.Sinks.PeriodicBatching;
+using Testcontainers.ArangoDb;
+
+namespace Core.Arango.Serilog.Tests;
+
+using Xunit;
+using Xunit.Abstractions;
+
+public class SerilogUnitTests : IAsyncLifetime
+{
+    private const string DatabaseName = "test";
+    private const string CollectionName = "logs";
+    
+    ArangoDbContainer ArangoDbContainer;
+    Func<IArangoConfiguration> CreateArangoConfiguration;
+
+    public async Task InitializeAsync()
+    {
+        ArangoDbContainer = new ArangoDbBuilder().Build();
+        await ArangoDbContainer.StartAsync();
+
+        CreateArangoConfiguration = () => new ArangoConfiguration
+        {
+            ConnectionString =
+                $"Server={ArangoDbContainer.GetTransportAddress()};User=root;Realm=CI-{Guid.NewGuid():D};Password=password;",
+        };
+    }
+
+    public async Task DisposeAsync()
+    {
+        await ArangoDbContainer.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task DefaultDatabaseIsCreated()
+    {
+        var config = CreateArangoConfiguration();
+        var arango = new ArangoContext(config);
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Sink(new PeriodicBatchingSink(
+                new ArangoSerilogSink(arango,
+                    "logs",
+                    "logs"),
+                new()
+                {
+                    BatchSizeLimit = 1000,
+                    QueueLimit = 100000,
+                    Period = TimeSpan.FromSeconds(2),
+                    EagerlyEmitFirstEvent = true,
+                }
+            ))
+            .MinimumLevel.Debug()
+            .CreateLogger();
+        
+        Assert.True(await arango.Database.ExistAsync("logs"));
+        Assert.True(await arango.Collection.ExistAsync("logs","logs"));
+    }
+    
+    [Fact]
+    public async Task DatabaseAndIndexesAreCreated()
+    {
+        var config = CreateArangoConfiguration();
+        var arango = new ArangoContext(config);
+        
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Sink(new PeriodicBatchingSink(
+                new ArangoSerilogSink(arango,
+                    DatabaseName,
+                    CollectionName,
+                    ArangoSerilogSink.LoggingRenderStrategy.StoreTemplate,
+                    true,
+                    true,
+                    true),
+                new()
+                {
+                    BatchSizeLimit = 1000,
+                    QueueLimit = 100000,
+                    Period = TimeSpan.FromSeconds(2),
+                    EagerlyEmitFirstEvent = true,
+                }
+            ))
+            .MinimumLevel.Debug()
+            .CreateLogger();
+
+        var indexes = await arango.Index.ListAsync(DatabaseName, CollectionName);
+        
+        Assert.True(await arango.Database.ExistAsync(DatabaseName));
+        Assert.True(await arango.Collection.ExistAsync(DatabaseName, CollectionName));
+
+        Assert.Contains(indexes!, x => x.Name == nameof(ArangoSerilogSink.LogEventEntity.Level));
+        Assert.Contains(indexes!, x => x.Name == nameof(ArangoSerilogSink.LogEventEntity.Timestamp));
+        Assert.Contains(indexes!, x => x.Name == nameof(ArangoSerilogSink.LogEventEntity.MessageTemplate));
+    }
+}
